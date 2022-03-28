531cb8af73155013c9499be6cfbd3925e35e9a38
aishahsofea <aishah.s.hanifa@gmail.com>
2022-03-28 22:49:51 +0800

    connect to NextJS

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,134 @@
+import { ethers } from "ethers";
+import React, { useEffect, useState } from "react";
+import PrimaryButton from "../components/primary-button";
+import abi from "../utils/Keyboards.json";
+
 export default function Home() {
-  return <p>The start of our app!</p>
+  const [ethereum, setEthereum] = useState(undefined);
+  const [connectedAccount, setConnectedAccount] = useState(undefined);
+  const [keyboards, setKeyboards] = useState([]);
+  const [newKeyboard, setNewKeyboard] = useState("");
+
+  const contractAddress = "0x05d190520AAD5323c86f681228be154E4200b999";
+  const contractABI = abi.abi;
+
+  const handleAccounts = (accounts) => {
+    if (accounts.length > 0) {
+      const account = accounts[0];
+      console.log(`We have an authorized account: ${account}`);
+      setConnectedAccount(account);
+    } else {
+      console.log("No authorized accounts yet");
+    }
+  };
+
+  const getConnectedAccount = async () => {
+    if (window.ethereum) {
+      setEthereum(window.ethereum);
+    }
+
+    if (ethereum) {
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+      handleAccounts(accounts);
+    }
+  };
+
+  const getKeyboards = async () => {
+    if (ethereum && connectedAccount) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const keyboardsContract = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        signer
+      );
+
+      const keyboards = await keyboardsContract.getKeyboards();
+      console.log("Retrieved keyboards...", keyboards);
+      setKeyboards(keyboards);
+    }
+  };
+
+  const submitCreate = async (e) => {
+    e.preventDefault();
+
+    if (!ethereum) {
+      console.error("Ethereum object is required to create a keyboard");
+      return;
+    }
+
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const keyboardsContract = new ethers.Contract(
+      contractAddress,
+      contractABI,
+      signer
+    );
+
+    const createTxn = await keyboardsContract.create(newKeyboard);
+    console.log(`Create transaction started... ${createTxn.hash}`);
+
+    await createTxn.wait();
+    console.log(`Created keyboard: ${createTxn.hash}`);
+
+    await getKeyboards();
+  };
+
+  useEffect(() => getConnectedAccount(), []);
+
+  useEffect(() => getKeyboards(), [connectedAccount]);
+
+  const connectAccount = async () => {
+    if (!ethereum) {
+      alert("MetaMask is required to connect an account");
+      return;
+    }
+
+    const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+    handleAccounts(accounts);
+  };
+
+  if (!ethereum) {
+    return <p>Please install MetaMask to connect to this site</p>;
+  }
+
+  if (!connectedAccount) {
+    return (
+      <PrimaryButton onClick={connectAccount}>
+        Connect MetaMask Wallet
+      </PrimaryButton>
+    );
+  }
+
+  return (
+    <div className="flex flex-col gap-y-8">
+      <form className="flex flex-col gap-y-2">
+        <div>
+          <label
+            htmlFor="keyboard-description"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Keyboard Description
+          </label>
+        </div>
+        <input
+          name="keyboard-type"
+          className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+          value={newKeyboard}
+          onChange={(e) => {
+            setNewKeyboard(e.target.value);
+          }}
+        />
+        <PrimaryButton type="submit" onClick={submitCreate}>
+          Create Keyboard!
+        </PrimaryButton>
+
+        <div>
+          {keyboards.map((keyboard, i) => (
+            <p key={i}>{keyboard}</p>
+          ))}
+        </div>
+      </form>
+    </div>
+  );
 }
